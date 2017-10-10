73c22bd30d3733cf9d9b463a56671dc11ef46dbc
Michael Lewis <mike@lew42.com>
2017-10-10 05:21:07 -0500

    yea

---
+++ simple/index.js
@@ -0,0 +1,37 @@
+// var concat = require("concat");
+var chokidar = require("chokidar");
+var fs = require("fs");
+
+// var File = require("../File");
+
+// File("./define.js")
+// 	.concat(File("./log.js"))
+// 	.concat(");")
+// 	.concat(File("./auto/*").join())
+// 	.write("../lew42.github.io/simple.js");
+
+var update = function(){
+	console.log("updating...");
+	var bundle = "";
+	var define = fs.readFileSync(__dirname + "/define.js");
+	var log = fs.readFileSync(__dirname + "/log.js");
+	bundle = define + log + ");" + "\r\n"; 
+	
+	var files = fs.readdirSync(__dirname + "/auto/");
+
+	files = files.map(file => __dirname + "/auto/" + file);
+	files.forEach((file) => bundle += "\r\n\r\n\r\n/*" + file + "*/\r\n" + fs.readFileSync(file));
+	// concat(["./define.js"].concat(files)).then((str) => {
+	// 	str += log + ");"
+	// 	fs.writeFileSync("../lew42.github.io/simple.js", str);
+	// });
+	fs.writeFileSync(__dirname + "/../lew42.github.io/simple.js", bundle);
+	// console.log("done updating");
+};
+
+
+
+chokidar.watch(__dirname).on("change", function() {
+	// console.log('changed', arguments);
+	update();
+});
