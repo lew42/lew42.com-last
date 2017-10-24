63564082fe45fe4534b76fff847588bca2880c8b
Michael Lewis <mike@lew42.com>
2017-10-24 16:20:52 -0500

    submoduling define2

--- define2/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-
-gulp.task("default", function(){
-	gulp.src(["one", "two"])
-		.pipe(concat("three", { newLine: "\r\n" }))
-		.pipe(gulp.dest("./"))
-});
