a010b9182c7efe87efa74034011321d5b19dab12
Michael Lewis <mike@lew42.com>
2018-06-07 08:59:30 -0500

    yea

---
+++ modules/tests/imports/index.js
@@ -0,0 +1,11 @@
+import View, { el } from "./View.js";
+
+function doit(){
+	var el = "el";
+}
+console.log(el);
+doit();
+// const el = View.el;
+// console.log(new View());
+
+// console.log(package);
